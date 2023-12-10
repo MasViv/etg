f9f2c4142d0fca59233b81abb84d4e5a089ad835
MasViv <112710248+MasViv@users.noreply.github.com>
2023-12-10 13:52:54 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,82 @@
+song1="";
+song2="";
+leftWristX=0;
+leftWristY=0;
+rightWristX=0;
+rightWristY=0;
+scoreLeftWrist=0;
+scoreRightWrist=0;
+
+function preload()
+{
+    song1=loadSound("music.mp3");
+    song2=loadSound("music2.mp3");
+}
+
+function setup()
+{
+    canvas=createCanvas(600,500);
+    canvas.center();
+
+    video=createCapture(VIDEO);
+    video.hide();
+
+    poseNet=ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses)
+}
+
+function modelLoaded()
+{
+    console.log("PNIO");
+}
+
+function draw()
+{
+    image(video,0,0,600,500);
+
+    fill("red");
+    stroke("green");
+    strokeWeight(5);
+
+    if (scoreLeftWrist>0.2)
+    {
+    circle(leftWristX,leftWristY,15);
+    InNumberleftWristY=Number(leftWristY);
+    remove_decimal=Math.floor(InNumberleftWristY);
+    if (leftWristY>RightWristY&&LeftWristY>0)
+    {
+        song2.stop();
+        song1.play();
+        document.getElementById("volume").innerHTML="Da sOnG Is sOnG 1, yea idk what this song accually is";
+    }
+    }
+
+    if (scoreRightWrist>0.2)
+    {
+    circle(RightWristX,RightWristY,15);
+    InNumberRightWristY=Number(RightWristY);
+    remove_decimal=Math.floor(InNumberRightWristY);
+    if (RightWristY>LeftWristY&&RightWristY>0)
+    {
+        song1.stop();
+        song2.play();
+        document.getElementById("volume").innerHTML="Da sOnG Is sOnG 2, yea idk what this song accually is";
+    }
+    }
+}
+
+function gotPoses(results)
+{
+    if (results.length>0)
+    {
+        console.log(results);
+        scoreLeftWrist=results[0].pose.keypoints[9].score;
+        console.log("ScOrElEfTwRiSt is" + scoreLeftWrist);
+        leftWristX=results[0].pose.leftWrist.x;
+        leftWristY=results[0].pose.leftWrist.y;
+        console.log("LWX=" + leftWristX + "LWY" + leftWristY);
+        rightWristX=results[0].pose.rightWrist.x;
+        rightWristY=results[0].pose.rightWrist.y;
+        console.log("RWX=" + rightWristX + "RWY" + rightWristY);
+    }
+}
